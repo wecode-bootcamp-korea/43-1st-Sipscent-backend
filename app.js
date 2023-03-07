213ac43682c15f38e98aefc38dbef4a0d85ffd61
trtw9889 <trtw9889@gmail.com>
2023-03-07 13:47:10 +0900

    [43기 임수정]Backend 초기세팅(수정사항 반영)

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const http = require("http");
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const appDataSource = require("./api/models/dataSource");
+const route = require("./api/routes");
 
 app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
+app.use(route);
+
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => {
+    console.error("Error during Data Source initialization", error);
+  });
 
 app.get("/ping", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
 
-const server = http.createServer(app);
 const PORT = process.env.PORT;
 
-const start = async () => {
-  try {
-    server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-start();
+app.listen(PORT, () => {
+  console.log(`server is listening on ${PORT}`);
+});
