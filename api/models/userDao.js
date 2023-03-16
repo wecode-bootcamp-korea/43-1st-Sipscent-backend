68f0007353d80cab897a868da7f20bb39e737731
trtw9889 <trtw9889@gmail.com>
2023-03-16 11:21:48 +0900

    [43기 임수정]회원가입 이메일 중복확인 수정

--- api/models/userDao.js
+++ api/models/userDao.js
@@ -12,6 +12,23 @@ const createUser = async (name, email, password, point) => {
   );
 };
 
+const checkRegisterdEmail = async (email) => {
+  const [result] = await appDataSource.query(
+    `
+    SELECT EXISTS (
+      SELECT 
+        id
+      FROM users
+      WHERE email = ?
+    ) as registerd
+  `,
+    [email]
+  );
+
+  return !!parseInt(result.registerd);
+};
+
 module.exports = {
   createUser,
+  checkRegisterdEmail,
 };
--- api/services/userService.js
+++ api/services/userService.js
@@ -16,8 +16,17 @@ const signUp = async (name, email, password) => {
   await emailValidation(email);
   await passwordValidation(password);
 
+  const checkEmail = await userDao.checkRegisterdEmail(email);
+
+  if (checkEmail) {
+    const error = new Error("이미 가입된 이메일입니다.");
+    error.statusCode = 400;
+
+    throw error;
+  }
+
   const hashPassword = await makePassword(password);
-  const point = 100000;
+  const point = 900000;
 
   return userDao.createUser(name, email, hashPassword, point);
 };
